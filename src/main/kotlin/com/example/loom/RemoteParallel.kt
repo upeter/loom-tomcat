e40ea7a9f1ad0805feb7611d87c2ae3fbfd2dd8c
upeter <peter.urs@gmail.com>
2022-07-04 21:25:20 +0200

    Demo start

--- src/main/kotlin/com/example/loom/RemoteParallel.kt
+++ src/main/kotlin/com/example/loom/RemoteParallel.kt
@@ -2,9 +2,9 @@ package com.example.loom
 
 import com.example.loom.domain.Avatar
 import com.example.loom.domain.Info
-import com.example.loom.dsl.async
-import com.example.loom.dsl.await
-import com.example.loom.dsl.virtualScope
+//import com.example.loom.dsl.async
+//import com.example.loom.dsl.await
+//import com.example.loom.dsl.virtualScope
 import com.example.utils.*
 import jakarta.servlet.annotation.WebServlet
 import jakarta.servlet.http.HttpServlet
@@ -16,16 +16,6 @@ import org.springframework.web.client.getForEntity
 
 @WebServlet("/parallel")
 class RemoteParallel : HttpServlet() {
-    override fun doGet(request: HttpServletRequest, response: HttpServletResponse) = virtualScope {
-        val delay = request.getParameter("delay").toLong()
-        val reply1 = async { restTemplate.getForEntity<Avatar>("/avatar?delay=$delay").body() }
-        val reply2 = async { restTemplate.getForEntity<Avatar>("/avatar?delay=$delay").body() }
-        with(response) {
-            contentType = "application/json"
-            mapper.writeValue(writer, Info(delay, reply1.await(), reply2.await()))
-        }
-
-
-    }
+    override fun doGet(request: HttpServletRequest, response: HttpServletResponse) = TODO("")
 
 }
--- src/main/kotlin/com/example/loom/dsl/LoomDsl.kt
+++ src/main/kotlin/com/example/loom/dsl/LoomDsl.kt
@@ -5,24 +5,4 @@ import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 import java.util.concurrent.Future
 
-fun <T> virtualScope(propagate:Boolean = true, block:StructuredTaskScope.ShutdownOnFailure.() -> T) {
-    StructuredTaskScope.ShutdownOnFailure().use{scope ->
-        scope.block()
-        scope.join()
-        if(propagate) scope.throwIfFailed()
-    }
-}
-
-fun <T> StructuredTaskScope.ShutdownOnFailure.async(block: () -> T):Future<T> =
-    fork(block)
-
-
-fun <T> Future<T>.await():T  = this.get()
-
-fun <T> List<Future<T>>.awaitAll():List<T>  = this.map { it.await() }
-
-
-fun <T> ExecutorService.async(block: () -> T):Future<T> {
-    return this.submit(block)
-}
 
